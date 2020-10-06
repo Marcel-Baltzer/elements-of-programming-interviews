079b8ec82bb0a7f9664cc29b163c6111155507fc
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2020-10-06 17:56:58 +0200

    finish parity class

--- PrimitiveTypes/Parity/Parity.cs
+++ PrimitiveTypes/Parity/Parity.cs
@@ -1,21 +1,24 @@
+using System;
+using System.Collections.Generic;
+
 namespace Elements_of_Programming_Interviews.PrimitiveTypes
 {
     public class Parity
     {
-        public int WithBruteForceFor(int number){
-            var result = 0;
+        public short WithBruteForceFor(long number){
+            long result = 0;
 
             while (number > 0){
-                result ^= number & 1;
+                result ^= (number & 1);
                 number >>= 1;
             }
 
-            return result;
+            return (short)result;
         }
 
 
-        public int For(int number){
-            var result = 0;
+        public short For(long number){
+            short result = 0;
 
             while (number > 0){
                 result ^= 1;
@@ -23,6 +26,38 @@ namespace Elements_of_Programming_Interviews.PrimitiveTypes
             }
 
             return result;
+        }
+
+        public short WithMaskFor(long number){
+             
+            var wordSize = 16;
+            var bitMask = 0xFFFF;
+
+            var precomputedParity = new short[1 << wordSize];
+
+            for (int i = 0; i < (1 << wordSize); ++i) {
+                precomputedParity[i] = For(i);
+            } 
+
+            var result = (short)(
+                precomputedParity[(int)((number >> (3 * wordSize)) & bitMask)]
+                ^ precomputedParity[(int)((number >> (2 * wordSize)) & bitMask)]
+                ^ precomputedParity[(int)((number >> wordSize) & bitMask)]
+                ^ precomputedParity[(int)(number & bitMask)]);
+
+            return result;
+        }
+
+        public short WithXorFor(long number){            
+
+            number ^= number >> 32;
+            number ^= number >> 16;
+            number ^= number >> 8;
+            number ^= number >> 4;
+            number ^= number >> 2;
+            number ^= number >> 1;
+
+            return (short)(number & 0x1);
         }
     }
 }
--- PrimitiveTypes/Parity/ParityTests.cs
+++ PrimitiveTypes/Parity/ParityTests.cs
@@ -14,7 +14,7 @@ namespace Elements_of_Programming_Interviews.PrimitiveTypes
         [InlineData(5,0)]
         [InlineData(6,0)]
         [InlineData(7,1)]
-        public void WithBruteForceFor(int number, int expectedResult){
+        public void WithBruteForceFor(long number, short expectedResult){
 
             var parity = new Parity();
 
@@ -32,11 +32,47 @@ namespace Elements_of_Programming_Interviews.PrimitiveTypes
         [InlineData(5,0)]
         [InlineData(6,0)]
         [InlineData(7,1)]
-        public void For(int number, int expectedResult){
+        public void For(long number, short expectedResult){
 
             var parity = new Parity();
 
             var result = parity.For(number);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(1,1)]
+        [InlineData(2,1)]
+        [InlineData(3,0)]
+        [InlineData(4,1)]
+        [InlineData(5,0)]
+        [InlineData(6,0)]
+        [InlineData(7,1)]
+        public void WithMaskFor(long number, short expectedResult){
+
+            var parity = new Parity();
+
+            var result = parity.WithMaskFor(number);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(1,1)]
+        [InlineData(2,1)]
+        [InlineData(3,0)]
+        [InlineData(4,1)]
+        [InlineData(5,0)]
+        [InlineData(6,0)]
+        [InlineData(7,1)]
+        public void WithXorFor(long number, short expectedResult){
+
+            var parity = new Parity();
+
+            var result = parity.WithXorFor(number);
 
             result.Should().Be(expectedResult);
         }
