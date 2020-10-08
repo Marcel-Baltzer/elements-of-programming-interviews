fdcd4bda60f3d8bf9646a47228abec1a8d43e9f2
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2020-10-08 10:57:50 +0200

    add array bootcamp

---
+++ 02. Arrays/00. BootCamp/Solution.cs
@@ -0,0 +1,26 @@
+namespace Elements_of_Programming_Interviews.Arrays.BootCamp
+{
+    public class Solution
+    {
+        public static void EvenOdd(int[] array)
+        {
+            int nextEven = 0;
+            int nextOdd = array.Length -1;
+
+            while (nextEven < nextOdd)
+            {
+                if(array[nextEven] % 2 == 0)
+                {
+                    nextEven++;
+                } 
+                else
+                {
+                    int temp = array[nextEven];
+                    array[nextEven] = array[nextOdd];
+                    array[nextOdd] = temp;
+                    nextOdd--;
+                }
+            }
+        }
+    }
+}
---
+++ 02. Arrays/00. BootCamp/Tests.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews.Arrays.BootCamp
+{
+    public class Tests
+    {
+        [Fact]
+        public void EvenOdd()
+        {
+            var array = new int[] {1, 2, 5, 6, 8, 4, 1};
+            var expectedArray = new int[] {4, 2, 8, 6, 5, 1, 1};
+
+            Solution.EvenOdd(array);
+
+            array.Should().BeEquivalentTo(expectedArray);
+        }
+    }
+}
