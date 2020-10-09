58f0c2479fcaac75f66711b5042887adcbbc9ce9
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2020-10-09 11:11:04 +0200

    rename folder

--- 02. Arrays/05. DeleteDuplicates/Solution.cs
+++ 02. Arrays/05. DeleteDuplicates/Solution.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Elements_of_Programming_Interviews.Arrays.DeleteDuplications
+namespace Elements_of_Programming_Interviews.Arrays.DeleteDuplicates
 {
     public class Solution
     {
--- 02. Arrays/05. DeleteDuplicates/Tests.cs
+++ 02. Arrays/05. DeleteDuplicates/Tests.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
-namespace Elements_of_Programming_Interviews.Arrays.DeleteDuplications
+namespace Elements_of_Programming_Interviews.Arrays.DeleteDuplicates
 {
     public class Tests
     {
