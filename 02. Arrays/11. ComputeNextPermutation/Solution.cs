8676c41dd11de6aa70cc09271f104d4ac3f6c583
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2020-10-10 22:38:05 +0200

    add compute next permutation

---
+++ 02. Arrays/11. ComputeNextPermutation/Solution.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+
+namespace Elements_of_Programming_Interviews.Arrays.ComputeNextPermutation
+{
+    public class Solution
+    {
+        static void Swap<T>(List<T> list, int i, int j)
+        {
+            T temp;
+            temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+
+        public static List<int> NextPermutation(List<int> permutation)
+        {
+            int k = permutation.Count - 2;
+            while (k >=0 && permutation[k] >= permutation[k+1])
+            {
+                k--;
+            }
+
+            if(k == -1)
+            {
+                // permutation is the last possible permutation.
+                return new List<int>(); 
+            }
+
+            // Swap the smallest entry after index k that is greater than permutation[k].
+            // We exploit the fact that permutation.subList(k + 1, permutation.Count) is decreasing
+            // so if we search in reverse order, the first entry that is greater than
+            // permutation[k] is the smallest such entry.
+
+            for (int i = permutation.Count -1; i > k ; i--)
+            {
+                if(permutation[i] > permutation[k])
+                {
+                    Swap<int>(permutation, k, i);
+                    break;
+                }
+            }
+
+            // Since permutation.subList[k + 1, permutation.Count) is in decreasing order, 
+            // we can build the smallest dictionary ordering of this subarray by reversing it.
+            permutation.Reverse(k+1, permutation.Count-k-1);
+
+            return permutation;
+        }
+    }
+}
---
+++ 02. Arrays/11. ComputeNextPermutation/Tests.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews.Arrays.ComputeNextPermutation
+{
+    public class Tests
+    {
+        [Fact]
+        public void NextPermutation()
+        {
+            var permutation = new List<int>() {6,2,1,5,4,3,0};
+
+            var result = Solution.NextPermutation(permutation);
+
+            var expectedPermutation = new List<int>() {6,2,3,0,1,4,5};
+
+            result.Should().BeEquivalentTo(expectedPermutation, options => options.WithStrictOrdering());
+        }
+    }
+}
