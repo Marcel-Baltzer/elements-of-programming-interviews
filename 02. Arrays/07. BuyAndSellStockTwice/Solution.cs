28db393172518b828087fa7d553f2337382e5a44
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2020-10-10 04:14:50 +0200

    add buy and sell stock twice

---
+++ 02. Arrays/07. BuyAndSellStockTwice/Solution.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace Elements_of_Programming_Interviews.Arrays.BuyAndSellStockTwice
+{
+    public class Solution
+    {
+        public static double BuyAndSellStockTwice(List<double> prices)
+        {
+            double maxTotalProfit = 0.0;
+            var firstBuySellProfits = new List<double>();
+            double minPriceSoFar = double.MaxValue;
+
+            // Forward phase. 
+            // For each day, we record maximum profit if we sell on that day.
+
+            for (int i = 0; i < prices.Count; i++)
+            {
+                minPriceSoFar = Math.Min(minPriceSoFar, prices[i]);
+                maxTotalProfit = Math.Max(maxTotalProfit, prices[i] - minPriceSoFar);
+                firstBuySellProfits.Add(maxTotalProfit);
+            }
+
+            // Backward phase. 
+            // For each day, find the maximum profit if we make the second buy on that day.
+
+            double maxPriceSoFar = double.MinValue;
+
+            for(int i = prices.Count -1; i > 0; i--)
+            {
+                maxPriceSoFar = Math.Max(maxPriceSoFar, prices[i]);
+                maxTotalProfit = Math.Max(maxTotalProfit, maxPriceSoFar - prices[i] + firstBuySellProfits[i-1]);
+            }
+
+            return maxTotalProfit;
+
+        }
+    }
+}
---
+++ 02. Arrays/07. BuyAndSellStockTwice/Tests.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews.Arrays.BuyAndSellStockTwice
+{
+    public class Tests
+    {
+        [Fact]
+        public void BuyAndSellStockTwice()
+        {
+            var prices = new List<double>() {12,11,13,9,12,8,14,13,15};
+
+            var result = Solution.BuyAndSellStockTwice(prices);
+
+            result.Should().Be(10);
+        }
+    }
+}
