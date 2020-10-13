c7abe4b3c801639c382afcaf8fcfc06ce630cefc
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2020-10-13 12:44:37 +0200

    add NonuniformRandomNumbers

---
+++ 02. Arrays/16. NonuniformRandomNumbers/Solution.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Elements_of_Programming_Interviews.Arrays.NonuniformRandomNumbers
+{
+    public class Solution
+    {
+public static int NonuniformRandomNumberGeneration(List<int> values, List<double> probabilities)
+        {
+            var prefixSumOfProbabilities = new List<double>();
+            prefixSumOfProbabilities.Add(0.0);
+
+            // Creating the endpoints for the intervals 
+            // corresponding to the probabilities.
+            foreach(var p in probabilities)
+            {
+                prefixSumOfProbabilities.Add(prefixSumOfProbabilities.Last() + p);
+            }
+
+            var r = new Random();
+            // Get a random number in [0.0,1.0).
+            var uniform01 = r.NextDouble();
+            // Find the index of the interval that uniformSIl lies in.
+            int it = prefixSumOfProbabilities.BinarySearch(uniform01);
+
+            if(it < 0)
+            {
+                // We want the index of the first element in the array which is
+                // greater than the key.
+                //
+                // When a key is not present in the array, BinarySearch()
+                // returns the negative of 1 plus the smallest index whose entry
+                // is greater than the key.
+                //
+                // Therefore , if the return value is negative , by taking its absolute
+                // value and adding 1 to it , we get the desired index.
+                var intervalldx = (Math.Abs(it) - 1) - 1;
+                return values[intervalldx];
+            }
+            else
+            {
+                // We have it >= 0, i.e., uniform0l equals an entry
+                // in prefixSumOfProbabilities.
+                //
+                // Because we uniform0l is a random double, the probability of it
+                // equalling an endpoint in prefixSumOfProbabilities is exceedingly low.
+                // However, it is not 0, so to be robust we must consider this case.
+                return values[it];
+            }
+        }
+    }
+}
---
+++ 02. Arrays/16. NonuniformRandomNumbers/Tests.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews.Arrays.NonuniformRandomNumbers
+{
+    public class Tests
+    {
+        [Fact]
+        public void NonuniformRandomNumberGeneration()
+        {
+            var values = new List<int>() {3, 5, 7, 11};
+            var probabilities = new List<double>() {9.0/18, 6.0/18, 2.0/18, 1.0/18};
+
+            var result = Solution.NonuniformRandomNumberGeneration(values, probabilities);
+
+            values.Should().Contain(result); 
+        }
+    }
+}
