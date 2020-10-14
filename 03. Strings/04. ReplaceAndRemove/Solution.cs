d14a8c7a93d98bdf1f159d4a93b12c90e564c5e1
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2020-10-14 18:56:44 +0200

    add replace and remove

---
+++ 03. Strings/04. ReplaceAndRemove/Solution.cs
@@ -0,0 +1,45 @@
+namespace Elements_of_Programming_Interviews.Strings.ReplaceAndRemove
+{
+    public class Solution
+    {
+        public static int ReplaceAndRemove(int size, char[] s)
+        {
+            // Forward iteration: remove "b"s and count the number of "a"s.
+            int writeIdx = 0;
+            int aCount = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                if(s[i] != 'b')
+                {
+                    s[writeIdx++] = s[i];
+                }
+                if(s[i] == 'a')
+                {
+                    aCount++;
+                }
+            }
+
+            // Backward iteration: replace "a"s with "dd"s starting from the end
+            int curIdx = writeIdx -1;
+            writeIdx = writeIdx + aCount -1;
+
+            int finalSize = writeIdx +1;
+            while(curIdx >= 0)
+            {
+                if(s[curIdx] == 'a')
+                {
+                    s[writeIdx--] = 'd';
+                    s[writeIdx--] = 'd';
+                }
+                else
+                {
+                    s[writeIdx--] = s[curIdx];
+                }
+                curIdx--;
+            }
+
+            return finalSize;
+        }
+    }
+}
---
+++ 03. Strings/04. ReplaceAndRemove/Tests.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews.Strings.ReplaceAndRemove
+{
+    public class Tests
+    {
+        [Fact]
+        public void ReplaceAndRemove()
+        {
+            var array = new char[] {'a','b','a','c',' '};
+
+            var result = Solution.ReplaceAndRemove(4, array);
+
+            var expectedArray = new char[] {'d','d','d','d','c'};
+
+            result.Should().Be(5);
+            array.Should().BeEquivalentTo(expectedArray, options => options.WithoutStrictOrdering());
+        }
+    }
+}
