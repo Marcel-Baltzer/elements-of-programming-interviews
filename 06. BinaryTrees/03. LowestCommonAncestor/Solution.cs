89561710710f55c1b6ae00eaba4734c632f87f96
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2021-03-30 23:25:41 +0200

    rename variable

--- 06. BinaryTrees/03. LowestCommonAncestor/Solution.cs
+++ 06. BinaryTrees/03. LowestCommonAncestor/Solution.cs
@@ -14,26 +14,26 @@ namespace Elements_of_Programming_Interviews.BinaryTrees.LowestCommonAncestor
             public BinaryTreeNode<int> ancestor { get; set; }
         }
 
-        public static BinaryTreeNode<int> LCA(BinaryTreeNode<int> tree, BinaryTreeNode<int> nodeLeft, BinaryTreeNode<int> nodeRight)
+        public static BinaryTreeNode<int> LCA(BinaryTreeNode<int> tree, BinaryTreeNode<int> node0, BinaryTreeNode<int> node1)
         {
-            return LcaHelper(tree, nodeLeft, nodeRight).ancestor;
+            return LcaHelper(tree, node0, node1).ancestor;
         }
 
-        private static Status LcaHelper(BinaryTreeNode<int> tree, BinaryTreeNode<int> nodeLeft, BinaryTreeNode<int> nodeRight)
+        private static Status LcaHelper(BinaryTreeNode<int> tree, BinaryTreeNode<int> node0, BinaryTreeNode<int> node1)
         {
             if (tree == null)
             {
                 return new Status(0, null);
             }
 
-            Status leftResult = LcaHelper(tree.Left, nodeLeft, nodeRight);
+            Status leftResult = LcaHelper(tree.Left, node0, node1);
             if (leftResult.NumTargetNodes == 2)
             {
                 // Found both nodes in the left subtree.
                 return leftResult;
             }
 
-            Status rightResult = LcaHelper(tree.Right, nodeLeft, nodeRight);
+            Status rightResult = LcaHelper(tree.Right, node0, node1);
             {
                 if (rightResult.NumTargetNodes == 2)
                 {
@@ -43,7 +43,7 @@ namespace Elements_of_Programming_Interviews.BinaryTrees.LowestCommonAncestor
             }
 
             int numTargetNodes = leftResult.NumTargetNodes + rightResult.NumTargetNodes
-                                + (tree == nodeLeft ? 1 : 0) + (tree == nodeRight ? 1 : 0);
+                                + (tree == node0 ? 1 : 0) + (tree == node1 ? 1 : 0);
 
             return new Status(numTargetNodes, numTargetNodes == 2 ? tree : null);
         }
