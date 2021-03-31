739829dd44e4b047b1a58c0fd2147e9c5f5633a1
Marcel-Baltzer <marcel.baltzer@hotmail.de>
2021-03-31 12:19:52 +0200

    add find root to leaf path

--- 06. BinaryTrees/06. FindRootToLeafPath/README.md
+++ 06. BinaryTrees/06. FindRootToLeafPath/README.md
@@ -1,1 +1,5 @@
 ## 6. Find a root to leaf path with specified sum
+
+Write a program which takes as input an integer and a binary tree with integer node weights, and checks if there exists a leaf whose path weight equals the given integer.
+
+> Hint: What do you need to know about the rest of the tree when checking a specific subtree?
---
+++ 06. BinaryTrees/06. FindRootToLeafPath/Solution.cs
@@ -0,0 +1,29 @@
+namespace Elements_of_Programming_Interviews.BinaryTrees.FindRootToLeafPath
+{
+    public class Solution
+    {
+        public static bool HasPathSum(BinaryTreeNode<int> tree, int targetSum)
+        {
+            return HasPathSumHelper(tree, 0, targetSum);
+        }
+
+        private static bool HasPathSumHelper(BinaryTreeNode<int> node, int partialPathSum, int targetSum)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            partialPathSum += node.Data;
+
+            if (node.Left == null && node.Right == null) // Leaf.
+            {
+                return partialPathSum == targetSum;
+            }
+
+            // Non-leaf .
+            return HasPathSumHelper(node.Left, partialPathSum, targetSum)
+                || HasPathSumHelper(node.Right, partialPathSum, targetSum);
+        }
+    }
+}
