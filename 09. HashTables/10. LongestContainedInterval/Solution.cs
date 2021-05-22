12637a5430a46d5453e7714f4b5997537be2a04f
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-22 21:46:07 +0200

    add HashTables LongestContainedInterval

---
+++ 09. HashTables/10. LongestContainedInterval/Solution.cs
@@ -0,0 +1,47 @@
+
+using System;
+using System.Collections.Generic;
+using Castle.Core.Internal;
+
+namespace Elements_of_Programming_Interviews.HashTables.LongestContainedInterval
+{
+    public class Solution
+    {
+        public static int LongestContainedRange(List<int> A)
+        {
+            // unprocessedEntries records the existence of each entry in A.
+            var unprocessedEntries = new HashSet<int>(A);
+
+            var maxIntervalSize = 0;
+
+            while (!unprocessedEntries.IsNullOrEmpty())
+            {
+                using var enumerator  = unprocessedEntries.GetEnumerator();
+                enumerator.MoveNext();
+                var a = enumerator.Current;
+                
+                unprocessedEntries.Remove(a);
+                
+                // Finds the lower bound of the largest range containing a.
+                var lowerBound = a - 1;
+                while (unprocessedEntries.Contains(lowerBound))
+                {
+                    unprocessedEntries.Remove(lowerBound);
+                    --lowerBound;
+                }
+                
+                // Finds the upper bound of the largest range containing a.
+                var upperBound = a + 1;
+                while (unprocessedEntries.Contains(upperBound))
+                {
+                    unprocessedEntries.Remove(upperBound);
+                    ++upperBound;
+                }
+
+                maxIntervalSize = Math.Max(upperBound - lowerBound - 1, maxIntervalSize);
+            }
+
+            return maxIntervalSize;
+        }
+    }
+}
---
+++ 09. HashTables/10. LongestContainedInterval/Tests.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews.HashTables.LongestContainedInterval
+{
+    public class Tests
+    {
+        [Fact]
+        public void LongestContainedRange()
+        {
+            var list = new List<int>() { 3,-2,7,9,8,1,2,0,-1,5,8 };
+
+            var result = Solution.LongestContainedRange(list);
+
+            result.Should().Be(6);
+        }
+    }
+}
