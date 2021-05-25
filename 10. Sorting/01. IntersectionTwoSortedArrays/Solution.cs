aeb071f7e6e65d4da09b764931b304a5d59731d1
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-25 18:24:51 +0200

    add Sorting IntersectionTwoSortedArrays

---
+++ 10. Sorting/01. IntersectionTwoSortedArrays/Solution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Elements_of_Programming_Interviews._10._Sorting._01._IntersectionTwoSortedArrays
+{
+    public class Solution
+    {
+        public static List<int> IntersectTwoSortedArraysWithEquals(List<int> A, List<int> B)
+        {
+            var intersectionAB = new List<int>();
+
+            for (var i = 0; i < A.Count; ++i)
+            {
+                if (i == 0 || A[i] != A[i - 1])
+                {
+                    foreach (var b in B)
+                    {
+                        if (A[i].Equals(b))
+                        {
+                            intersectionAB.Add(A[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return intersectionAB;
+        }
+
+        public static List<int> IntersectTwoSortedArraysWithBinarySearch(List<int> A, List<int> B)
+        {
+            var intersectionAB = new List<int>();
+
+            for (var i = 0; i < A.Count; i++)
+            {
+                if ((i == 0 || A[i] != A[i - 1])
+                    && B.BinarySearch(A[i]) >= 0)
+                {
+                    intersectionAB.Add(A[i]);
+                }
+            }
+
+            return intersectionAB;
+        }
+        
+        public static List<int> IntersectTwoSortedArrays(List<int> A, List<int> B)
+        {
+            var intersectionAB = new List<int>();
+            
+            int i = 0, j = 0;
+            while (i < A.Count && j < B.Count)
+            {
+                if (A[i] == B[j] && (i == 0 || A[i] != A[i - 1]))
+                {
+                    intersectionAB.Add(A[i]);
+                    ++i;
+                    ++j;
+                }
+                else if (A[i] < B[j])
+                {
+                    ++i;
+                }
+                else // A.get(i) > B.get(j).
+                {
+                    ++j;
+                }
+            }
+
+            return intersectionAB;
+        }
+    }
+}
---
+++ 10. Sorting/01. IntersectionTwoSortedArrays/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews._10._Sorting._01._IntersectionTwoSortedArrays
+{
+    public class Tests
+    {
+        [Fact]
+        public void IntersectTwoSortedArraysWithEquals()
+        {
+            var a = new List<int>() {2, 3, 3, 5, 5, 6, 7, 7, 8, 12};
+            var b = new List<int>() {5, 5, 6, 8, 8, 9, 10, 10};
+
+            var result = Solution.IntersectTwoSortedArraysWithEquals(a, b);
+
+            var expectedResult = new List<int>() {5, 6, 8};
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+        
+        [Fact]
+        public void IntersectTwoSortedArraysWithBinarySearch()
+        {
+            var a = new List<int>() {2, 3, 3, 5, 5, 6, 7, 7, 8, 12};
+            var b = new List<int>() {5, 5, 6, 8, 8, 9, 10, 10};
+
+            var result = Solution.IntersectTwoSortedArraysWithBinarySearch(a, b);
+
+            var expectedResult = new List<int>() {5, 6, 8};
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+        
+        [Fact]
+        public void IntersectTwoSortedArrays()
+        {
+            var a = new List<int>() {2, 3, 3, 5, 5, 6, 7, 7, 8, 12};
+            var b = new List<int>() {5, 5, 6, 8, 8, 9, 10, 10};
+
+            var result = Solution.IntersectTwoSortedArrays(a, b);
+
+            var expectedResult = new List<int>() {5, 6, 8};
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
