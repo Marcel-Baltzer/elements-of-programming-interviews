ecbb5152261b9712c066a6837160be7cf1526d55
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-26 17:57:40 +0200

    add Sorting BootCamp

---
+++ 10. Sorting/00. BootCamp/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Elements_of_Programming_Interviews._10._Sorting._00._BootCamp
+{
+    public class Solution
+    {
+        public class Student : IComparable
+        {
+            public readonly string Name;
+            public readonly double GradePointAverage;
+
+            public Student(string name, double gradePointAverage)
+            {
+                Name = name;
+                GradePointAverage = gradePointAverage;
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (obj is Student that)
+                {
+                    return string.Compare(Name, that.Name, StringComparison.Ordinal);
+                }
+
+                return -1;
+            }
+        }
+
+        public static void SortByName(List<Student> students)
+        {
+            students.Sort();
+        }
+
+        public static void SortByGradePointAverage(List<Student> students)
+        {
+            students.Sort((a, b) => a.GradePointAverage.CompareTo(b.GradePointAverage));
+        }
+    }
+}
---
+++ 10. Sorting/00. BootCamp/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews._10._Sorting._00._BootCamp
+{
+    public class Tests
+    {
+        [Fact]
+        public void SortByName()
+        {
+            var list = new List<Solution.Student>
+            {
+                new ("A", 2),
+                new ("C", 4),
+                new ("E", 6),
+                new ("B", 3),
+                new ("D", 1)
+            };
+
+            Solution.SortByName(list);
+            
+            var expectedResult = new List<Solution.Student>
+            {
+                new ("A", 2),
+                new ("B", 3),
+                new ("C", 4),
+                new ("D", 1),
+                new ("E", 6)
+            };
+
+            list.Should().BeEquivalentTo(expectedResult, options => options.WithStrictOrdering());
+        }
+        
+        [Fact]
+        public void SortByGradePointAverage()
+        {
+            var list = new List<Solution.Student>
+            {
+                new ("A", 2),
+                new ("C", 4),
+                new ("E", 6),
+                new ("B", 3),
+                new ("D", 1)
+            };
+            
+            Solution.SortByGradePointAverage(list);
+            
+            var expectedResult = new List<Solution.Student>
+            {
+                new ("D", 1),
+                new ("A", 2),
+                new ("B", 3),
+                new ("C", 4),
+                new ("E", 6)
+            };
+
+            list.Should().BeEquivalentTo(expectedResult, options => options.WithStrictOrdering());
+        }
+    }
+}
