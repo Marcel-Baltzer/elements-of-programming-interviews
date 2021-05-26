5c272e4e9630b000aa45501a0a227a335ca742b3
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-26 16:05:33 +0200

    add Sorting SalaryThreshold

---
+++ 10. Sorting/11. SalaryThreshold/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Elements_of_Programming_Interviews._10._Sorting._11._SalaryThreshold
+{
+    public class Solution
+    {
+        public static double FindSalaryCap(double targetPayroll, List<double> currentSalaries)
+        {
+            currentSalaries.Sort();
+            var unadjustedSalarySum = 0.0;
+
+            for (var i = 0; i < currentSalaries.Count; i++)
+            {
+                var adjustedSalarySum = currentSalaries[i] * (currentSalaries.Count - i);
+
+                if (unadjustedSalarySum + adjustedSalarySum >= targetPayroll)
+                {
+                    return (targetPayroll - unadjustedSalarySum) / (currentSalaries.Count - i);
+                }
+
+                unadjustedSalarySum += currentSalaries[i];
+            }
+            
+            // No solution, since targetPayroll > existing payroll.
+            return -1.0;
+        }
+    }
+}
---
+++ 10. Sorting/11. SalaryThreshold/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews._10._Sorting._11._SalaryThreshold
+{
+    public class Tests
+    {
+        [Fact]
+        public void FindSalaryCap()
+        {
+            var list = new List<double>() {90, 30, 100, 40, 20};
+
+            var result = Solution.FindSalaryCap(210, list);
+
+            result.Should().Be(60);
+        }
+        
+        [Fact]
+        public void FindSalaryCapWithNoSolution()
+        {
+            var list = new List<double>() {90, 30, 0, 40, 20};
+
+            var result = Solution.FindSalaryCap(210, list);
+
+            result.Should().Be(-1);
+        }
+    }
+}
