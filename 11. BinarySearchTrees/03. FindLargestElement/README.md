b4eaa5905a5af1882b9f5279035f030867fde495
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-28 18:47:25 +0200

    add BinarySearchTrees FindLargestElement

---
+++ 11. BinarySearchTrees/03. FindLargestElement/README.md
@@ -0,0 +1,7 @@
+## 3. Find the k largest elements in a binary search tree
+
+A binary search tree is a sorted data structure, which suggests that it should be possible to find the k largest keys easily.
+
+Write a program that takes as input a binary search tree and an integer k, and returns the k largest elements in the binary search tree in decreasing order.
+
+> Hint: What does an inorder traversal yield?
---
+++ 11. BinarySearchTrees/03. FindLargestElement/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Elements_of_Programming_Interviews.Types;
+
+namespace Elements_of_Programming_Interviews._11._BinarySearchTrees._03._FindLargestElement
+{
+    public class Solution
+    {
+        public static List<int> FindKLargestInBinarySearchTree(BinarySearchTreeNode<int> tree, int k)
+        {
+            var kLargestElements = new List<int>();
+            FindKLargestInBinarySearchTreeHelper(tree, k, kLargestElements);
+            return kLargestElements;
+        }
+
+        private static void FindKLargestInBinarySearchTreeHelper(BinarySearchTreeNode<int> tree, int k, ICollection<int> kLargestElements)
+        {
+            // Perform reverse inorder traversal.
+            if (tree != null && kLargestElements.Count < k)
+            {
+                FindKLargestInBinarySearchTreeHelper(tree.Right, k, kLargestElements);
+                if (kLargestElements.Count < k)
+                {
+                    kLargestElements.Add(tree.Data);
+                    FindKLargestInBinarySearchTreeHelper(tree.Left, k, kLargestElements);
+                }
+            }
+        }
+    }
+}
