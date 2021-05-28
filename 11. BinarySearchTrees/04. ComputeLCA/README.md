30ba4818c4b66a1c0aaea6fc64e8554b190665b4
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-28 19:02:17 +0200

    add BinarySearchTrees ComputeLCA

---
+++ 11. BinarySearchTrees/04. ComputeLCA/README.md
@@ -0,0 +1,3 @@
+## 4. Compute the lowest common ancestor in a binary search tree
+
+Design an algorithm that takes as input a binary search tree and two nodes, and returns the lowest common ancestor of the two nodes. Assume all keys are distinct. Nodes do not have references to their parents.
---
+++ 11. BinarySearchTrees/04. ComputeLCA/Solution.cs
@@ -0,0 +1,32 @@
+﻿using Elements_of_Programming_Interviews.Types;
+
+namespace Elements_of_Programming_Interviews._11._BinarySearchTrees._04._ComputeLCA
+{
+    public class Solution
+    {
+        // Input nodes are not null and the key at s is less than or equal to that at b.
+        public static BinarySearchTreeNode<int> FindLowestCommonAncestor(BinarySearchTreeNode<int> tree, 
+                                                                         BinarySearchTreeNode<int> smallKey, 
+                                                                         BinarySearchTreeNode<int> bigKey)
+        {
+            var p = tree;
+
+            while (p.Data < smallKey.Data || p.Data > bigKey.Data)
+            {
+                // Keep searching since p is outside of [s, b].
+                while (p.Data < smallKey.Data)
+                {
+                    p = p.Right; // LCA must be in p’s right child.
+                }
+
+                while (p.Data > bigKey.Data)
+                {
+                    p = p.Left; // LCA must be in p’s left child.
+                }
+            }
+            
+            //Now, s.data >= p.data && p.data <- b.data.
+            return p;
+        }
+    }
+}
