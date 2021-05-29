b9d4f21b7872d4a27813bb3b6c90e0e0d7457b61
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-29 21:19:36 +0200

    add BinarySearchTrees RangeLookupProblem

---
+++ 11. BinarySearchTrees/12. RangeLookupProblem/README.md
@@ -0,0 +1,5 @@
+## 12. The range lookup problem
+
+Write a program that takes as input a binary search tree and an interval and returns the binary search tree keys that lie in the interval.
+
+> Hint: How many edges are traversed when the successor function is repeatedly called m times?
---
+++ 11. BinarySearchTrees/12. RangeLookupProblem/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Elements_of_Programming_Interviews.Types;
+
+namespace Elements_of_Programming_Interviews._11._BinarySearchTrees._12._RangeLookupProblem
+{
+    public class Solution
+    {
+        public class Interval
+        {
+            public readonly int Left;
+            public readonly int Right;
+
+            public Interval(int left, int right)
+            {
+                Left = left;
+                Right = right;
+            }
+        }
+
+        public static List<int> RangeLookupInBinarySearchTree(BinarySearchTreeNode<int> tree, Interval interval)
+        {
+            var result = new List<int>();
+            RangeLookUpInBinarySearchTreeHelper(tree, interval, result);
+            return result;
+        }
+
+        private static void RangeLookUpInBinarySearchTreeHelper(BinarySearchTreeNode<int> tree, Interval interval,
+            ICollection<int> result)
+        {
+            if (tree == null)
+            {
+                return;
+            }
+
+            if (interval.Left <= tree.Data && tree.Data <= interval.Right)
+            {
+                RangeLookUpInBinarySearchTreeHelper(tree.Left, interval, result);
+                result.Add(tree.Data);
+                RangeLookUpInBinarySearchTreeHelper(tree.Right, interval, result);
+            }
+            else if (interval.Left > tree.Data)
+            {
+                RangeLookUpInBinarySearchTreeHelper(tree.Right, interval, result);
+            }
+            else // interval.right >= tree.data
+            {
+                RangeLookUpInBinarySearchTreeHelper(tree.Left, interval, result);
+            }
+        }
+    }
+}
