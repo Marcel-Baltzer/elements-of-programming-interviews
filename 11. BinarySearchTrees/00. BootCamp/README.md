e88953a967e3d85b794768f331c44ecd65ea2b94
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-30 18:46:51 +0200

    add BinarySearchTrees BootCamp

---
+++ 11. BinarySearchTrees/00. BootCamp/README.md
@@ -0,0 +1,3 @@
+## 0. BootCamp
+
+The following program demonstrates how to check if a given value is present in a binary search tree. It is a nice illustration of the power of recursion when operating on binary search trees.
---
+++ 11. BinarySearchTrees/00. BootCamp/Solution.cs
@@ -0,0 +1,19 @@
+﻿using Elements_of_Programming_Interviews.Types;
+
+namespace Elements_of_Programming_Interviews._11._BinarySearchTrees._00._BootCamp
+{
+    public class Solution
+    {
+        public static BinarySearchTreeNode<int> SearchBinarySearchTree(BinarySearchTreeNode<int> tree, int key)
+        {
+            if (tree == null || tree.Data == key)
+            {
+                return tree;
+            }
+
+            return key < tree.Data
+                ? SearchBinarySearchTree(tree.Left, key)
+                : SearchBinarySearchTree(tree.Right, key);
+        }
+    }
+}
