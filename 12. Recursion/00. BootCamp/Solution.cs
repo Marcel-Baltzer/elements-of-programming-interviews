52edf993879382cb8747890afb86a6e5ce1cca4e
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-05-31 17:44:55 +0200

    add Recursion BootCamp

---
+++ 12. Recursion/00. BootCamp/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace Elements_of_Programming_Interviews._12._Recursion._00._BootCamp
+{
+    public class Solution
+    {
+        public static long GreatestCommonDivisor(long x, long y)
+        {
+            return y == 0 ? x : GreatestCommonDivisor(y, x % y);
+        }
+    }
+}
---
+++ 12. Recursion/00. BootCamp/Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews._12._Recursion._00._BootCamp
+{
+    public class Tests
+    {
+        [Fact]
+        public void GreatestCommonDivisor()
+        {
+            var result = Solution.GreatestCommonDivisor(156, 36);
+
+            result.Should().Be(12);
+        }
+    }
+}
