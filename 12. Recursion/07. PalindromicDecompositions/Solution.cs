ad78dde1adf23604da834ee1f8da3eb52f6285c7
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-06-01 15:24:30 +0200

    add Recursion PalindromicDecomposition

---
+++ 12. Recursion/07. PalindromicDecompositions/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Elements_of_Programming_Interviews._12._Recursion._07._PalindromicDecompositions
+{
+    public static class Solution
+    {
+        public static IEnumerable<List<string>> PalindromePartitioning(string input)
+        {
+            var result = new List<List<string>>();
+            DirectedPalindromePartitioning(input, 0, new List<string>(), result);
+            return result;
+        }
+
+        private static void DirectedPalindromePartitioning(string input, int offset,
+            IList<string> partialPartition, ICollection<List<string>> result)
+        {
+            if (offset == input.Length)
+            {
+                result.Add(new List<string>(partialPartition));
+                return;
+            }
+
+            for (var i = offset + 1; i <= input.Length; ++i)
+            {
+                var prefix = input.Substring(offset, i - offset);
+
+                if (IsPalindrome(prefix))
+                {
+                    partialPartition.Add(prefix);
+                    DirectedPalindromePartitioning(input, i, partialPartition, result);
+                    partialPartition.RemoveAt(partialPartition.Count - 1);
+                }
+            }
+        }
+
+        private static bool IsPalindrome(string prefix)
+        {
+            var j = prefix.Length - 1;
+            for (var i = 0; i < j; ++i, --j)
+            {
+                if (prefix[i] != prefix[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 12. Recursion/07. PalindromicDecompositions/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Elements_of_Programming_Interviews._12._Recursion._07._PalindromicDecompositions
+{
+    public class Tests
+    {
+        [Fact]
+        public void PalindromePartitioning()
+        {
+            var result = Solution.PalindromePartitioning("0204451881");
+
+            var expectedResult = new List<List<string>>
+            {
+                new() {"0", "2", "0", "4", "4", "5", "1", "8", "8", "1"},
+                new() {"0", "2", "0", "4", "4", "5", "1", "88", "1"},
+                new() {"0", "2", "0", "4", "4", "5", "1881"},
+                new() {"0", "2", "0", "44", "5", "1", "8", "8", "1"},
+                new() {"0", "2", "0", "44", "5", "1", "88", "1"},
+                new() {"0", "2", "0", "44", "5", "1881"},
+                new() {"020", "4", "4", "5", "1", "8", "8", "1"},
+                new() {"020", "4", "4", "5", "1", "88", "1"},
+                new() {"020", "4", "4", "5", "1881"},
+                new() {"020", "44", "5", "1", "8", "8", "1"},
+                new() {"020", "44", "5", "1", "88", "1"},
+                new() {"020", "44", "5", "1881"}
+            };
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
